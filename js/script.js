2f1a17d5f394e5ac34f691959080361b64d187df
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-03-10 20:57:53 +0200

    Fine esercizio.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // jS regole della pagina
 var generaBtn = document.getElementById('genera');
-var annullaBtn = document.getElementById('anulla');
+var annullaBtn = document.getElementById('annulla');
 var costo = 0.21;
 var costoTotale = 0;
 var min = 90000;
@@ -13,6 +13,7 @@ generaBtn.addEventListener('click', function() {
   console.log(name, distanza, eta);
   if (name !== '' && distanza > 0 && eta !== ''){
 
+    // Costo Biglietto Standart
     costoTotale = costo * distanza;
 
     // lo sconto
@@ -20,23 +21,51 @@ generaBtn.addEventListener('click', function() {
       var sconto = costoTotale * 0.2;
       costoTotale = costoTotale - sconto;
       // console.log('minorenne', eta);
+      document.getElementById('eta-sconto').innerHTML = ' Sconto = 20%';
     }
     else if (eta == 3) {
       var sconto = costoTotale * 0.4;
       costoTotale = costoTotale - sconto;
       // console.log('over65', eta);
+      document.getElementById('eta-sconto').innerHTML = 'Sconto = 40%';
+    }
+    else {
+      document.getElementById('eta-sconto').innerHTML = '';
     }
 
-    document.getElementById('costo-totale').innerHTML = costoTotale + ' €';
+    // Codice CP
     var cp = Math.floor(Math.random() * max) + min;
+    document.getElementById('codice-cp').innerHTML = cp;
+
+    // Costo + Sconto
+    document.getElementById('costo-totale').innerHTML = costoTotale.toFixed(2) + ' €';
+
+    // Carrozza
+    var carrozza = Math.floor(Math.random() * 20) + 1;
+    document.getElementById('carrozza-num').innerHTML = carrozza;
+
+    // Nome Passeggero
+    document.getElementById('nome-passeggero').innerHTML = name;
+
+
 
   }
   else {
-    console.log('nah');
+    alert('Vi preghiamo di inserire tutti dati correttamente.');
   }
 
 });
 
+// Un modo per togliere tutto
 annullaBtn.addEventListener('click', function(){
+  var name = document.getElementById('name').value = "";
+  var distanza = document.getElementById("distance").value = "";
+  var eta = document.getElementById('eta').value = "";
+
+  document.getElementById("nome-passeggero").innerHTML = "";
+  document.getElementById("eta-sconto").innerHTML = "";
+  document.getElementById("carrozza-num").innerHTML = "";
+  document.getElementById("codice-cp").innerHTML = "";
+  document.getElementById("costo-totale").innerHTML = "";
 
 });
